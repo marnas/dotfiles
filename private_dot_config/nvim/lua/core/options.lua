3eeb101045c5efda7802ba335d0a4afe5525aee6
Marco Santonastaso <marco@santonastaso.com>
2023-03-05 19:04:53 +0000

    waybar improved

--- private_dot_config/nvim/lua/core/options.lua
+++ private_dot_config/nvim/lua/core/options.lua
@@ -29,6 +29,7 @@ local options = {
   relativenumber = false,                  -- set relative numbered lines
   numberwidth = 4,                         -- set number column width to 2 {default 4}
   signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
+  colorcolumn = "80",                      -- show vertical column
   wrap = false,                            -- display lines as one long line
   scrolloff = 8,                           -- is one of my fav
   sidescrolloff = 8,
